61c684bc3a964d664cb250de6d6ef7dc84c01977
Will Bruce <willtiger13@gmail.com>
2017-08-31 09:52:43 -0400

    created a few users to get started

---
+++ index.js
@@ -0,0 +1,14 @@
+const models = require('./models');
+
+models.User.create({
+  username: "joel",
+  password: "asdf"
+});
+models.User.create({
+  username: "sarah",
+  password: "asdf"
+});
+models.User.create({
+  username: "peanut",
+  password: "asdf"
+});
